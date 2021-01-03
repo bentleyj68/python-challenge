ffe7c49e1fb4c8ddac78e6688e4bdccb8b94231d
bentleyj68 <bentleyj68@gmail.com>
2021-01-04 06:31:17 +0800

    Sprint 2 and final results

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,23 +2,27 @@
 import os
 import csv
 
-# Function - Show results on screen and to a text file
+# Function - Show results on screen and write to a text file
 #          - Input parameter is a dictionary of the analysis
-def show_results(analysis_dict):
+def show_results(final_rslt):
+    
     # Specify the file to write the financial analysis result to
     output_path = os.path.join('analysis','results.txt')
+    
     # Store each line in a list for reuse
     results = []
     results.append('Financial Analysis')
     results.append('-------------------------')
-    results.append(f"Total Months: {analysis_dict['count']}")
-    results.append(f"Total: {analysis_dict['net_profit']}")
-    results.append(f"Avearge Change: {analysis_dict['average_difference']}")
-    results.append(f"Greatest Increase in Profits: {analysis_dict['biggest_increase']}")
-    results.append(f"Greatest Decrease in Profits: {analysis_dict['biggest_decrease']}")
+    results.append(f"Total Months: {final_rslt['count']}")
+    results.append(f"Total: ${final_rslt['net_profit']}")
+    results.append(f"Avearge Change: ${final_rslt['average_difference']}")
+    results.append(f"Greatest Increase in Profits: {final_rslt['increase_month']} (${final_rslt['biggest_increase']})")
+    results.append(f"Greatest Decrease in Profits: {final_rslt['decrease_month']} (${final_rslt['biggest_decrease']})")
+    
     # Print the results to screen
     for result in results:
         print(result)
+    
     # Open the file with write mode
     with open(output_path, 'w', newline='\n') as txtfile:
         # Print the results to a text file
@@ -33,17 +37,19 @@ with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
    
-    # Read the header row first
+    # Read the header row first and ignore it
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
     
     # Setup a Python Dictionary to store all the results 
     analysis = {"count": 0,
                 "net_profit":  0,
                 "average_difference" :0,
                 "biggest_increase": 0,
-                "biggest_decrease": 0}
+                "increase_month": "",
+                "biggest_decrease": 0,
+                "decrease_month": ""}
 
+    # loop through the csv and calculate the analysis
     for row in csvreader:
         analysis["count"] += 1
         analysis["net_profit"] = analysis["net_profit"] + int(row[1])
@@ -56,14 +62,15 @@ with open(csvpath) as csvfile:
             # Check if the new difference is a bigger increase or decrease
             if difference > analysis["biggest_increase"]:
                 analysis["biggest_increase"] = difference
+                analysis["increase_month"] = row[0]
             if difference < analysis["biggest_decrease"]:
                 analysis["biggest_decrease"] = difference 
-        # Store the current month for calculating the difference on the next month
+                analysis["decrease_month"] = row[0]
+        # Store the current month total - for calculating the difference on the next month
         previous = int(row[1])
 
-    # Use the sum of all differences to calculate the average, but ignore the first month
+    # Use the sum of all differences to calculate the average, but ignore the first month in the count
     analysis["average_difference"] = round(analysis["average_difference"] / (analysis["count"] -1), 2)
-    # print(analysis)
 
     # Call the show_results function to display and store the results from the analysis
     show_results(analysis)
